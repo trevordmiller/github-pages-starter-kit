362ef30b70d146e596b6d2f925844c16a0cbe000
Trevor D. Miller <trevordmiller@icloud.com>
2015-03-30 19:14:59 -0600

    Updating readme with generic info. Updating colors. Removing analytics.

--- _site/public/src/main.js
+++ _site/public/src/main.js
@@ -1,13 +1,9 @@
 import {header} from './components/header/_header.js';
-import {analytics} from './components/analytics/_analytics.js';
-import {browserUpdate} from './components/browser-update/_browser-update.js';
 
 document.addEventListener('DOMContentLoaded', () => {
 
 	// INIT COMPONENT MODULES
 	header.init();
-	analytics.init();
-	browserUpdate.init();
 
 	// INIT LIB MODULES
 	fitIt('[src*="youtube.com"]');
